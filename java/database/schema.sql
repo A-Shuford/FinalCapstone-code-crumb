c751d2b6a7858cecfad3535bae195652439aebb5
Rommel Soriano <rommelariel10@gmail.com>
2024-08-12 13:12:24 -0400

    update schema

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -168,13 +168,13 @@ INSERT INTO cake_price (cake_style_id, cake_size_id, has_writing, price) VALUES
 
 --INSERTING CAKES--
 INSERT INTO cake(cake_Name, cake_style, cake_size, cake_flavor, cake_filling, cake_frosting, cake_type, has_writing, custom_text, amount_available, cake_price, image_name) 
-VALUES('Chocolate Ganache Cake', 1, 2, 3, NULL, 2, 'Standard', true,'', 2, 2,'/src/assets/Cake_Images/standard_chocolate_ganache_cake.png'),
+VALUES
 ('Chocolate Ganache Cake', 1, 2, 3, NULL, 2, 'Standard', false,'', 2, 2,'/src/assets/Cake_Images/standard_chocolate_ganache_cake.png'),
-('Vanilla Buttercream Cake', 1, 2, 2, NULL, 1, 'Standard', true,'', 3, 2,'/src/assets/Cake_Images/standard_vanilla_cake.png'),
-('Funfetti cake', 1, 1, 6, NULL, 1, 'Standard', true,'', 1, 1,'/src/assets/Cake_Images/standard_funfetti_cake.jpg'),
-('Red Velvet Cake', 3, 2, 3, NULL, 1, 'Standard', true,'', 2, 8,'/src/assets/Cake_Images/standard_red_velvet_cake.png'),
-('Pound Cake', 2, 3, 1, NULL, 1, 'Standard', true, '', 2, 6,'/src/assets/Cake_Images/standard_pound_cake.png'),
-('Butterscotch Cake', 2, 3, 1, 7, 1, 'Standard', true, '', 2, 6, '/src/assets/Cake_Images/standard_butterscotch_cake.jpg');
+('Vanilla Buttercream Cake', 1, 2, 2, NULL, 1, 'Standard', false,'', 3, 2,'/src/assets/Cake_Images/standard_vanilla_cake.png'),
+('Funfetti cake', 1, 1, 6, NULL, 1, 'Standard', false,'', 1, 1,'/src/assets/Cake_Images/standard_funfetti_cake.jpg'),
+('Red Velvet Cake', 3, 2, 3, NULL, 1, 'Standard', false,'', 2, 8,'/src/assets/Cake_Images/standard_red_velvet_cake.png'),
+('Pound Cake', 2, 3, 1, NULL, 1, 'Standard', false, '', 2, 6,'/src/assets/Cake_Images/standard_pound_cake.png'),
+('Butterscotch Cake', 2, 3, 1, 7, 1, 'Standard', false, '', 2, 6, '/src/assets/Cake_Images/standard_butterscotch_cake.jpg');
 
 INSERT INTO cart_item_status(status_name)
 VALUES ('Pending'),
