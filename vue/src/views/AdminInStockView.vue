d6909eb24f92a886f911eb6a9e5540c6392d3984
Megan <166268768+megan-amurphy@users.noreply.github.com>
2024-08-12 19:26:11 -0400

    AdminStock updated

--- vue/src/views/AdminInStockView.vue
+++ vue/src/views/AdminInStockView.vue
@@ -1,73 +1,73 @@
 <template>
-    <header-vue />
-    <nav-bar-vue />
-    <section id="instock-cards">
-      <article
-        class="instock-card"
-        v-for="cake in cakes"
-        v-bind:key="cake.cakeId"
-      >
+  <header-vue />
+  <nav-bar-vue />
+  <section id="instock-cards">
+    <article
+      class="instock-card"
+      v-for="cake in cakes"
+      :key="cake.cakeId"
+    >
       <div class="id">{{ cake.cakeId }}</div>
-        <div class="price">{{ currency(cake.price) }}</div>
-        <div class="cake-name action" v-on:click="details(cake.cakeId)">
-          {{ cake.cakeName }}
-          {{ cake.amountAvailable }}
-        </div>
-      
-        <div class="cake-image">
-          <img v-bind:src="cake.imageName" />
-        </div>
-        <div class = "update" v-bind:to=" 'UpdatedAvailability'">
-      <td><label for="updateAvailable">Amount Available:</label></td>
-                    <td>
-                        <select v-model="cake.amountAvailable" id="cake.amountAvailable" required>
-                            <option value="0">0</option>
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-                            <option value="4">4</option>
-                            <option value="5">5</option>
-                        </select>
-                    </td>
-                    <img src="/public/update_icon.jpg" alt="Update Icon" class="icon action" v-on:click="updateAvailability(cake)" title="Update Availability" />
-                    </div>
-                  </article>
-    </section>
-    <footer-vue />
-  </template>
+      <div class="price">{{ currency(cake.price) }}</div>
+      <div class="cake-name action" @click="details(cake.cakeId)">
+        {{ cake.cakeName }} (Available: {{ cake.amountAvailable }})
+      </div>
+      <div class="cake-image">
+        <img :src="cake.imageName" alt="Cake Image" />
+      </div>
+      <div class="update">
+        <label for="updateAvailable">Amount Available:</label>
+        <select v-model="cake.amountAvailable" required>
+          <option value="0">0</option>
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+          <option value="5">5</option>
+        </select>
+        <img
+          src="/public/update_icon.jpg"
+          alt="Update Icon"
+          class="icon action"
+          @click="updateAvailability(cake)"
+          title="Update Availability"
+        />
+      </div>
+    </article>
+  </section>
+  <footer-vue />
+</template>
 
 <script>
 import inStock from "../services/InStockService.js";
-import InstockTable from "../components/InstockTable.vue";
-import InstockCards from "../components/InstockCards.vue";
 import HeaderVue from "../components/Header.vue";
 import NavBarVue from "../components/Navbar.vue";
-import MascotModalVue from "../components/MascotModal.vue";
 import FooterVue from "../components/Footer.vue";
 
 export default {
   components: {
-    InstockTable,
-    InstockCards,
     HeaderVue,
     NavBarVue,
-    MascotModalVue,
     FooterVue,
   },
-  
+
   data() {
     return {
       cakes: [],
       cardView: true,
     };
   },
-  methods :{
-    getCakes (){
-        inStock.getCakesAdmin().then((response) => {
+
+  methods: {
+    getCakes() {
+      inStock
+        .getCakesAdmin()
+        .then((response) => {
           this.cakes = response.data;
         })
         .catch(this.handleError("Getting in-stock cakes was unsuccessful"));
     },
+
     handleError(baseMessage) {
       return (error) => {
         const response = error.response;
@@ -77,35 +77,38 @@ export default {
         console.error(message);
       };
     },
+
     currency(value) {
-        return new Intl.NumberFormat(`en-US`, {
-          currency: `USD`,
-          style: "currency",
-        }).format(value);
-      },
-      updateAvailability(){
-        inStock.updateCakeAmountAvailableFromAdminStock(this.cake).then(() => {
+      return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(value);
+    },
+
+    updateAvailability(cake) {
+      inStock
+        .updateCakeAmountAvailableFromAdminStock(cake)
+        .then(() => {
           this.$store.commit(
             "SET_SUCCESS",
-            `Updated '${this.cake.cakeName}' availability`
+            `Updated '${cake.cakeName}' availability`
           );
         })
         .catch((error) => {
-            const response = error.response;
-            const message =
-              "Unable to update cake amount: " +
-              (response ? response.message : "Could not reach server");
-            this.$store.commit("SET_ERROR", message);
-            console.error(message);
-          });
-        
-      },
+          const response = error.response;
+          const message =
+            "Unable to update cake amount: " +
+            (response ? response.message : "Could not reach server");
+          this.$store.commit("SET_ERROR", message);
+          console.error(message);
+        });
+    },
 
-      details(id) {
-        this.$router.push({ name: "inStockDetails", params: { id: id} });
-      },
-    
+    details(id) {
+      this.$router.push({ name: "inStockDetails", params: { id: id } });
+    },
   },
+
   created() {
     this.getCakes();
   },
@@ -113,8 +116,7 @@ export default {
 </script>
 
 <style scoped>
- div.update img {
-    height: 30px;
-  }
-
+div.update img {
+  height: 30px;
+}
 </style>
