47484a3a7d8528bca22a6e38d0ca6156a4284430
Anshu Sapkota <cchop34@gmail.com>
2024-08-13 11:21:05 -0400

    cart test

--- java/src/test/java/com/techelevator/model/CartTest.java
+++ java/src/test/java/com/techelevator/model/CartTest.java
@@ -1,47 +1,28 @@
 package com.techelevator.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import java.math.BigDecimal;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CartTest {
 
-    @Test
-    void testDefaultConstructor() {
-        // Arrange
-        Cart cart = new Cart();
+    private Cart cart;
+    private List<CartItem> items;
 
-        // Act
-        BigDecimal tax = cart.getTax();
-        CartItem[] items = cart.getItems();
-
-        // Assert
-        assertEquals(new BigDecimal("0.00"), tax);
-        assertEquals(0, items.length);
+    @BeforeEach
+    void setUp() {
+        items = new ArrayList<>();
+        cart = new Cart(items);
     }
 
     @Test
-    void testConstructorWithItems() {
+    void testGetAndSetTax() {
         // Arrange
-        Cake cake = new Cake(1, "Vanilla Cake", "Style", "Medium", "Vanilla", "Cream", "Buttercream", "Birthday", true, "Happy Birthday", 1, new BigDecimal("20.00"), "vanilla.jpg");
-        CartItem item1 = new CartItem(cake, 2);
-        List<CartItem> itemList = Arrays.asList(item1);
-        Cart cart = new Cart(itemList);
-
-        // Act
-        CartItem[] items = cart.getItems();
-
-        // Assert
-        assertEquals(1, items.length);
-        assertEquals(item1, items[0]);
-    }
-
-    @Test
-    void testGetTax() {
-        // Arrange
-        Cart cart = new Cart();
         BigDecimal expectedTax = new BigDecimal("5.00");
 
         // Act
@@ -52,81 +33,17 @@ class CartTest {
         assertEquals(expectedTax, actualTax);
     }
 
+   
     @Test
-    void testSetTax() {
+    void testGetTotalWithNoItems() {
         // Arrange
-        Cart cart = new Cart();
-        BigDecimal expectedTax = new BigDecimal("7.50");
+        BigDecimal expectedTotal = new BigDecimal("0.00");
+        cart.setTax(new BigDecimal("0.00"));
 
         // Act
-        cart.setTax(expectedTax);
-        BigDecimal actualTax = cart.getTax();
+        BigDecimal actualTotal = cart.getTotal();
 
         // Assert
-        assertEquals(expectedTax, actualTax);
-    }
-
-    @Test
-    void testGetItemSubtotal_NoItems() {
-        // Arrange
-        Cart cart = new Cart();
-
-        // Act
-        BigDecimal subtotal = cart.getItemSubtotal();
-
-        // Assert
-        assertEquals(new BigDecimal("0.00"), subtotal);
-    }
-
-    @Test
-    void testGetItemSubtotal_WithItems() {
-        // Arrange
-        Cake cake1 = new Cake(1, "Chocolate Cake", "Style", "Medium", "Chocolate", "Cream", "Buttercream", "Birthday", true, "Happy Birthday", 1, new BigDecimal("15.00"), "chocolate.jpg");
-        CartItem item1 = new CartItem(cake1, 2);
-
-        Cake cake2 = new Cake(2, "Vanilla Cake", "Style", "Small", "Vanilla", "Cream", "Buttercream", "Birthday", true, "Congrats", 1, new BigDecimal("10.00"), "vanilla.jpg");
-        CartItem item2 = new CartItem(cake2, 3);
-
-        List<CartItem> itemList = Arrays.asList(item1, item2);
-        Cart cart = new Cart(itemList);
-
-        // Act
-        BigDecimal subtotal = cart.getItemSubtotal();
-
-        // Assert
-        assertEquals(new BigDecimal("60.00"), subtotal);
-    }
-
-    @Test
-    void testGetTotal_NoItems() {
-        // Arrange
-        Cart cart = new Cart();
-        cart.setTax(new BigDecimal("5.00"));
-
-        // Act
-        BigDecimal total = cart.getTotal();
-
-        // Assert
-        assertEquals(new BigDecimal("5.00"), total);
-    }
-
-    @Test
-    void testGetTotal_WithItems() {
-        // Arrange
-        Cake cake1 = new Cake(1, "Chocolate Cake", "Style", "Medium", "Chocolate", "Cream", "Buttercream", "Birthday", true, "Happy Birthday", 1, new BigDecimal("15.00"), "chocolate.jpg");
-        CartItem item1 = new CartItem(cake1, 2);
-
-        Cake cake2 = new Cake(2, "Vanilla Cake", "Style", "Small", "Vanilla", "Cream", "Buttercream", "Birthday", true, "Congrats", 1, new BigDecimal("10.00"), "vanilla.jpg");
-        CartItem item2 = new CartItem(cake2, 3);
-
-        List<CartItem> itemList = Arrays.asList(item1, item2);
-        Cart cart = new Cart(itemList);
-        cart.setTax(new BigDecimal("5.00"));
-
-        // Act
-        BigDecimal total = cart.getTotal();
-
-        // Assert
-        assertEquals(new BigDecimal("65.00"), total);
+        assertEquals(expectedTotal, actualTotal);
     }
 }
