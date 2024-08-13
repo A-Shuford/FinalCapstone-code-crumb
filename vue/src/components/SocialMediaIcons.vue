e220aba74f9621d9c4a0ca98d9da1521b32e39ab
Rommel Soriano <rommelariel10@gmail.com>
2024-08-13 16:04:27 -0400

    pushing the icons

--- vue/src/components/SocialMediaIcons.vue
+++ vue/src/components/SocialMediaIcons.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="social-media-icons">
     <a href="https://facebook.com" target="_blank" aria-label="Facebook">
-      <img src="src\assets\Social Media Icons\317727_facebook_social media_social_icon.png" alt="Facebook">
+      <img src="../assets/Social Media Icons/317727_facebook_social media_social_icon.png" alt="Facebook">
     </a>
     <a href="https://twitter.com" target="_blank" aria-label="Twitter">
-      <img src="src\assets\Social Media Icons\317723_social media_tweet_twitter_social_icon.png" alt="Twitter">
+      <img src="../assets/Social Media Icons/317723_social media_tweet_twitter_social_icon.png" alt="Twitter">
     </a>
     <a href="https://instagram.com" target="_blank" aria-label="Instagram">
-      <img src="src\assets\Social Media Icons\1298747_instagram_brand_logo_social media_icon.png" alt="Instagram">
+      <img src="../assets/Social Media Icons/1298747_instagram_brand_logo_social media_icon.png" alt="Instagram">
     </a>
     <a href="https://linkedin.com" target="_blank" aria-label="LinkedIn">
-      <img src="src\assets\Social Media Icons\5296501_linkedin_network_linkedin logo_icon.png" alt="LinkedIn">
+      <img src="../assets/Social Media Icons/5296501_linkedin_network_linkedin logo_icon.png" alt="LinkedIn">
     </a>
     <a href="https://pinterest.com" target="_blank" aria-label="Pinterest">
-      <img src="src\assets\Social Media Icons\5282545_pin_pinterest_inspiration_pinterest logo_icon.png" alt="Pinterest">
+      <img src="../assets/Social Media Icons/5282545_pin_pinterest_inspiration_pinterest logo_icon.png" alt="Pinterest">
     </a>
   </div>
 </template>
