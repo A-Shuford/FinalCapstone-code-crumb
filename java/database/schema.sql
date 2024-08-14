617db398194a684c66c5ede3389f9aceb24223e4
Megan <166268768+megan-amurphy@users.noreply.github.com>
2024-08-14 11:33:35 -0400

    Database edit????

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,15 +1,14 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users, cake_style, cake_size, cake_flavor, cake_filling, 
-cake_frosting, cart_item_status, cake, cart_item, cart_item_status, 
-cart_item_cake, cart_item_cake, cake_price;
+cake_frosting, cake, cart_item, cart_item_cake, cake_price, cart_item_status;
 
 CREATE TABLE users (
 	user_id SERIAL,
 	username varchar(50) NOT NULL UNIQUE,
 	yourName varchar(200) NOT NULL,
 	email varchar(50) NOT NULL,
-	phone_number varchar(12) NOT NULL,  
+	phone_number varchar(15) NOT NULL,  
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
@@ -88,7 +87,6 @@ CREATE TABLE cart_item (
 	status_id int DEFAULT 1,
 	pickup_date Date, 
 	pickup_time TIME, 
-	--pickupdate and picktime talk to team
 	CONSTRAINT pk_cart_item_id PRIMARY KEY(cart_item_id),
 	CONSTRAINT fk_user_id FOREIGN KEY(user_id) REFERENCES users(user_id),
 	CONSTRAINT fk_status_id FOREIGN KEY (status_id) REFERENCES cart_item_status(cart_item_status_id)
@@ -99,10 +97,10 @@ CREATE TABLE cart_item_cake (
     cart_item_id int NOT NULL,
     cake_id int NOT NULL,
     quantity int NOT NULL DEFAULT 1,
-   -- price decimal(8,2) NOT NULL,  -- Price of the cake at the time of order
     CONSTRAINT fk_cart_item FOREIGN KEY(cart_item_id) REFERENCES cart_item(cart_item_id),
     CONSTRAINT fk_cake FOREIGN KEY(cake_id) REFERENCES cake(cake_id)
 );
+
 ----INSERTING FROSTING TYPES---------
 INSERT INTO cake_frosting(frosting_name) VALUES ('Vanilla');
 INSERT INTO cake_frosting(frosting_name) VALUES ('Chocolate Ganache');
@@ -111,32 +109,33 @@ INSERT INTO cake_frosting(frosting_name) VALUES ('Strawberry');
 INSERT INTO cake_frosting(frosting_name) VALUES ('Mint');
 INSERT INTO cake_frosting(frosting_name) VALUES ('Cream cheese');
 
-------INSERTING FILLING TYPE----------
+------INSERTING FILLING TYPES----------
+INSERT INTO cake_filling(filling_name) VALUES ('None');
 INSERT INTO cake_filling(filling_name) VALUES ('Apple Jelly');
 INSERT INTO cake_filling(filling_name) VALUES ('Raspberry Jelly');
 INSERT INTO cake_filling(filling_name) VALUES ('Strawberry Jelly');
 INSERT INTO cake_filling(filling_name) VALUES ('Vanilla Custard');
-INSERT INTO cake_filling(filling_name) VALUES ('Chocolae Custard');
+INSERT INTO cake_filling(filling_name) VALUES ('Chocolate Custard');
 INSERT INTO cake_filling(filling_name) VALUES ('Cookie Dough');
 INSERT INTO cake_filling(filling_name) VALUES ('Caramel');
 INSERT INTO cake_filling(filling_name) VALUES ('Peanut Butter');
 
--------INSERTING FLAVOR---------
+-------INSERTING FLAVORS---------
 INSERT INTO cake_flavor(flavor_name) VALUES('Butterscotch');
 INSERT INTO cake_flavor(flavor_name) VALUES('Vanilla');
 INSERT INTO cake_flavor(flavor_name) VALUES('Chocolate');
 INSERT INTO cake_flavor(flavor_name) VALUES('Lemon');
-INSERT INTO cake_flavor(flavor_name) VALUES('Cookie & Cream');
+INSERT INTO cake_flavor(flavor_name) VALUES('Cookies & Cream');
 INSERT INTO cake_flavor(flavor_name) VALUES('Funfetti');
 INSERT INTO cake_flavor(flavor_name) VALUES('Pumpkin');
 INSERT INTO cake_flavor(flavor_name) VALUES('Key Lime');
 
---------INSTERING SIZE---------
+--------INSERTING SIZES---------
 INSERT INTO cake_size(size_name) VALUES('Small');
 INSERT INTO cake_size(size_name) VALUES('Medium');
 INSERT INTO cake_size(size_name) VALUES('Large');
 
-------INSERTING Style---------
+------INSERTING STYLES---------
 INSERT INTO cake_style(style_name) VALUES('Layered');
 INSERT INTO cake_style(style_name) VALUES('Sheet');
 INSERT INTO cake_style(style_name) VALUES('Cupcakes');
@@ -149,8 +148,8 @@ INSERT INTO cake_price (cake_style_id, cake_size_id, price) VALUES
 
 -- Prices for Sheet Cakes
 INSERT INTO cake_price (cake_style_id, cake_size_id, price) VALUES 
-(2, 1,  39.00),  -- Sheet, Small 1/4 
-(2, 2,  57.00),  -- Sheet, Medium 1/2
+(2, 1, 39.00),  -- Sheet, Small 1/4 
+(2, 2, 57.00),  -- Sheet, Medium 1/2
 (2, 3, 107.00); -- Sheet, Large Whole Sheet
 
 -- Prices for Cupcakes
@@ -194,7 +193,6 @@ INSERT INTO users (username, yourname, email, phone_number, password_hash, role)
 VALUES ('user', 'John Smith', 'user@example.com', '123-456-7890', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC', 'ROLE_USER'),
 ('admin', 'Admin Smith', 'admin@example.com', '098-765-4321', '$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN' );
 
-
 GRANT ALL
 ON ALL TABLES IN SCHEMA public
 TO final_capstone_owner;
@@ -210,6 +208,5 @@ TO final_capstone_appuser;
 GRANT USAGE, SELECT
 ON ALL SEQUENCES IN SCHEMA public
 TO final_capstone_appuser;
-
 
 COMMIT TRANSACTION;
